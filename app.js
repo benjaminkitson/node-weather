8c398be3d64945dc82617903919e7873bcea5e6f
Benjamin Kitson <benjaminkitson13@gmail.com>
2022-01-11 12:32:09 +0000

    added chalk, added error handling

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const request = require("postman-request")
+const request = require("postman-request");
+const chalk = require("chalk");
 
 
 //mapbox
@@ -15,23 +16,36 @@ let weather
 request({ json: true, url: location }, function (error, response) {
 
   if (error) {
-    console.log("Request failed")
+    console.log(chalk.red("Location request failed"))
     return
   }
 
   const data = response.body;
+
+  if (data.features.length === 0) {
+    console.log(chalk.red("Invalid request"))
+    return
+  }
+
   const coords = data.features[0].center.map((coord => coord.toFixed(4)));
   [longitude, latitude] = coords
+  console.log(chalk.green("Success!"))
   console.log(`Search query: ${process.argv[2]}`)
-  console.log(`Latitude: ${coords[1]}; Longitude: ${coords[0]}`)
+  console.log(`Latitude: ${chalk.green(coords[1])}; Longitude: ${chalk.green(coords[0])}`)
   weather = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=metric&appid=2faed7e8eafc113ae52daf1dc6e39ea2`
 
   //// weather request ////
 
   request({ json: true, url: weather }, function (error, response) {
+
+    if (error) {
+      console.log(chalk.red("Weather request failed"))
+      return
+    }
+
     const data = response.body;
     const temp = data.current.temp;
     const feelsLike = data.current.feels_like;
-    console.log(`It's ${temp} degrees outside, but it feels like ${feelsLike}.`)
+    console.log(`It's ${chalk.blue(temp)} degrees outside, but it feels like ${chalk.blue(feelsLike)}.`)
   })
 })
