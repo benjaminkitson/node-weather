5b0c080e889b69436fb316ff855293bb4f657aec
Benjamin Kitson <benjaminkitson13@gmail.com>
2022-01-11 12:16:18 +0000

    geocoder api connected, program now returns location coordinates and temperature information based on search query

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 const request = require("postman-request")
 
-const url = "https://api.openweathermap.org/data/2.5/onecall?lat=51.2829&lon=0.1682&units=metric&appid=2faed7e8eafc113ae52daf1dc6e39ea2"
 
-request({ json: true, url: url}, function(error, response) {
+//mapbox
+
+const search = process.argv[2]
+
+const location = `https://api.mapbox.com/geocoding/v5/mapbox.places/${search}.json?access_token=pk.eyJ1IjoiYmVuazEzIiwiYSI6ImNreWExNWFzYjAxM24ycW84dDZoNmp1eHoifQ.y2iTi6ZWriTeUu29Dwoszg&limit=1`
+
+let latitude, longitude;
+let weather
+
+//// location request ////
+
+request({ json: true, url: location }, function (error, response) {
   const data = response.body;
-  const temp = data.current.temp;
-  const feelsLike = data.current.feels_like;
+  const coords = data.features[0].center.map((coord => coord.toFixed(4)));
+  [longitude, latitude] = coords
+  console.log(`Search query: ${process.argv[2]}`)
+  console.log(`Latitude: ${coords[1]}; Longitude: ${coords[0]}`)
+  weather = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=metric&appid=2faed7e8eafc113ae52daf1dc6e39ea2`
 
-  console.log(`It's ${temp} degrees outside, but it feels like ${feelsLike}.`)
+  //// weather request ////
+
+  request({ json: true, url: weather }, function (error, response) {
+    const data = response.body;
+    const temp = data.current.temp;
+    const feelsLike = data.current.feels_like;
+    console.log(`It's ${temp} degrees outside, but it feels like ${feelsLike}.`)
+  })
 })
