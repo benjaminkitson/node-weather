e304df67514adf0a14cd697fc6195c68e39079ff
Benjamin Kitson <benjaminkitson13@gmail.com>
2022-01-21 16:42:27 +0000

    weather data prints to console on submission of form

--- public/js/client.js
+++ public/js/client.js
@@ -1,19 +1,21 @@
-fetch('http://localhost:3000/weather?location=tn132as')
-  .then(response => response.json())
-  .then(data => {
-    if (data.error) {
-      console.log(data.error)
-    } else {
-      console.log(data)
-    }
-  });
+function fetchWeather(url) {
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      if (data.error) {
+        console.log(data.error)
+      } else {
+        console.log(data)
+      }
+    });
+}
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 
-console.log(weatherForm)
 
-weatherForm.addEventListener('submit', (e) =>{
+weatherForm.addEventListener('submit', (e) => {
   e.preventDefault()
-  console.log(search.value)
+  const url = `http://localhost:3000/weather?location=${search.value}`
+  fetchWeather(url)
 })
