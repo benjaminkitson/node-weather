677cbb3e78af3132e22478d4e8927c4b3267e383
Benjamin Kitson <benjaminkitson13@gmail.com>
2022-01-23 18:02:17 +0000

    search bar has fancy animation

--- public/js/client.js
+++ public/js/client.js
@@ -11,6 +11,7 @@ function fetchWeather(url, func) {
 
 }
 
+const searchField = document.querySelector('.search-field')
 const searchButton = document.querySelector('.search-button')
 const searchContainer = document.querySelector('.search-field-container')
 const search = document.querySelector('input')
@@ -22,12 +23,25 @@ searchButton.addEventListener('mouseup', (e) => {
   if (!Array.from(searchButton.classList).includes('active')) {
     searchContainer.classList.add('expanded')
     searchButton.classList.add('active')
+    searchField.focus()
   } else {
     const url = `http://localhost:3000/weather?location=${search.value}`
     fetchWeather(url, (weather) => {
-      weatherItems.forEach((weatherItem, i) => {
+      console.log(weather)
+      if (typeof weather === 'string') {
+        weatherItems[0].innerHTML = weather
+      } else {
+        weatherItems.forEach((weatherItem, i) => {
         weatherItem.innerHTML = Object.values(weather.response)[i]
-      })
+        })
+      }
     })
+  }
+})
+
+searchField.addEventListener('blur', () => {
+  if (!searchField.value) {
+    searchContainer.classList.remove('expanded')
+    searchButton.classList.remove('active')
   }
 })
