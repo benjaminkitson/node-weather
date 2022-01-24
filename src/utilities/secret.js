e45cad34b0ed9d8babbeed402178aa8ceb45a29c
Benjamin Kitson <benjaminkitson13@gmail.com>
2022-01-24 17:36:30 +0000

    heroku variable success, deleted references to secrets.js

--- src/utilities/secret.js
+++
@@ -1,6 +0,0 @@
-const secret = {
-  GEOCODE: "pk.eyJ1IjoiYmVuazEzIiwiYSI6ImNreWExNWFzYjAxM24ycW84dDZoNmp1eHoifQ.y2iTi6ZWriTeUu29Dwoszg&limit=1",
-  WEATHER: "2faed7e8eafc113ae52daf1dc6e39ea2"
-}
-
-module.exports = secret
