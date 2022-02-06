fc18ff0dd6e4217fc12cd618ffd2103fb0d127de
Benjamin Kitson <benjaminkitson13@gmail.com>
2022-02-06 22:11:50 +0000

    started modifying page layout, refactored weather response into two objects

--- src/utilities/weather.js
+++ src/utilities/weather.js
@@ -13,13 +13,17 @@ function getWeather({latitude, longitude}, callback) {
       const {temp, feels_like:feelsLike, weather, sunrise, sunset, wind_speed:windSpeed} = weatherData.current;
       const formattedWeather = weather[0].description.charAt(0).toUpperCase() + weather[0].description.slice(1);
       callback(undefined, {
-        temp,
-        feelsLike,
-        weather: formattedWeather,
-        sunrise: new Date(sunrise*1000).toLocaleTimeString(),
-        sunset: new Date(sunset*1000).toLocaleTimeString(),
-        windSpeed,
-        icon: weather[0].icon,
+        topSection: {
+          icon: weather[0].icon,
+          weather: formattedWeather
+        },
+        bottomSection: {
+          temp,
+          feelsLike,
+          sunrise: new Date(sunrise * 1000).toLocaleTimeString(),
+          sunset: new Date(sunset * 1000).toLocaleTimeString(),
+          windSpeed,
+        }
       })
     }
   })
