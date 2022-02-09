3a936813ef6bf4e55aba0a1317896bfb2275fb7d
Benjamin Kitson <benjaminkitson13@gmail.com>
2022-02-09 11:21:30 +0000

    added lots of semicolons to app.js

--- src/app.js
+++ src/app.js
@@ -2,15 +2,15 @@ const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
 const request = require('postman-request');
-const getWeather = require('./utilities/weather.js')
-const getGeocode = require('./utilities/geocode.js')
+const getWeather = require('./utilities/weather.js');
+const getGeocode = require('./utilities/geocode.js');
 
-const app = express()
-const PORT = process.env.PORT || 3000
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.set('view engine', 'hbs')
-app.set('views', path.join(__dirname, '../templates/views'))
-hbs.registerPartials(path.join(__dirname, '../templates/partials'))
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, '../templates/views'));
+hbs.registerPartials(path.join(__dirname, '../templates/partials'));
 
 
 app.use(express.static(
@@ -20,40 +20,40 @@ app.use(express.static(
 app.get('', (req, res) => {
   res.render('index.hbs', {
     title: "Weather!"
-  })
-})
+  });
+});
 
 app.get('/weather', (req, res) => {
   if (!req.query.location) {
     return res.send({
       error: "You must provide a location"
-    })
+    });
   }
 
   getGeocode(req.query.location, (error, response = {}) => {
     if (error) {
-      return res.send({error})
+      return res.send({error});
     }
 
     getWeather(response, (error, response) => {
       if (error) {
-        return res.send({error})
+        return res.send({error});
       }
 
       res.send({
         response
-      })
-    })
-  })
+      });
+    });
+  });
 
-})
+});
 
 app.get('*', (req, res) => {
   res.render('404', {
     message: "Just a general fail really"
-  })
-})
+  });
+});
 
 app.listen(PORT, () => {
   console.log("Server started!")
-})
+});
